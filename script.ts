4f703ca771140db474fefa4c32d9353cae83a74a
bem130 <070617haruki@gmail.com>
2023-07-11 15:11:35 +0900

    import change to parse

--- script.ts
+++ script.ts
@@ -1,17 +1,4 @@
-import * as mfm from 'https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/index.js';
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@2022.7.21-dev.1/built/index.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@2022.7.21-dev.1/built/api.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@2022.7.21-dev.1/built/node.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@2022.7.21-dev.1/built/internal/parser.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@2022.7.21-dev.1/built/internal/util.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@2022.7.21-dev.1/built/internal/index.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@2022.7.21-dev.1/built/internal/core/index.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/index.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/api.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/node.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/internal/index.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/internal/parser.js";
-import * from "https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/internal/util.js";
+import {parse} from 'https://cdn.jsdelivr.net/npm/mfm-js@0.23.3/built/index.js';
 const inputText =
     `<center>
 Hello $[tada everynan! :tada:]
@@ -20,10 +7,10 @@ https://google.com/
 </center>`;
 
 // Generate a MFM tree from the full MFM text.
-const mfmTree = mfm.parse(inputText);
+const mfmTree = parse(inputText);
 console.log(mfmTree);
 // Generate a MFM tree from the simple MFM text.
-const simpleMfmTree = mfm.parseSimple('I like the hot soup :soup:');
-console.log(simpleMfmTree);
-// Reverse to a MFM text from the MFM tree.
-const text = mfm.toString(mfmTree);
+// const simpleMfmTree = mfm.parseSimple('I like the hot soup :soup:');
+// console.log(simpleMfmTree);
+// // Reverse to a MFM text from the MFM tree.
+// const text = mfm.toString(mfmTree);
