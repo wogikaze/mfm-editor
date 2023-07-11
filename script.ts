decbe9f7f9cf4f20314f1a8f2f0805a066c6ccf0
bem130 <070617haruki@gmail.com>
2023-07-11 14:00:12 +0900

    add: script.ts

---
+++ script.ts
@@ -0,0 +1,16 @@
+import * as mfm from "https://github.com/misskey-dev/mfm.js/raw/develop/src/index.ts"
+const inputText = 
+`<center>
+Hello $[tada everynan! :tada:]
+@ai
+https://google.com/
+</center>`;
+
+// Generate a MFM tree from the full MFM text.
+const mfmTree = mfm.parse(inputText);
+ console.log(mfmTree);
+// Generate a MFM tree from the simple MFM text.
+const simpleMfmTree = mfm.parseSimple('I like the hot soup :soup:​');
+ console.log(simpleMfmTree);
+// Reverse to a MFM text from the MFM tree.
+const text = mfm.toString(mfmTree);
